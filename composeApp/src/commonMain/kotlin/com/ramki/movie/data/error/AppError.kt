9a9b28f2a650864a2f6ce16b6acd27f028bc75a4
Ramki Anbazhagan <ramki613@gmail.com>
2025-01-09 23:32:55 +0700

    App error updated

--- composeApp/src/commonMain/kotlin/com/ramki/movie/data/error/AppError.kt
+++ composeApp/src/commonMain/kotlin/com/ramki/movie/data/error/AppError.kt
@@ -2,5 +2,5 @@ package com.ramki.movie.data.error
 
 open class AppError(
     override val cause: Throwable?,
-    open val readableMessage: String? = null,
-) : Throwable(readableMessage, cause)
+    override val message: String? = null,
+) : Throwable(message, cause)
