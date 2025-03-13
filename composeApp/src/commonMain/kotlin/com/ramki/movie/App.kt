ecacd499d2c5282f8280fba1e55db85718cbc295
Ramki Anbazhagan <ramki613@gmail.com>
2025-03-13 19:34:58 +0700

    App screen updated, SplashScreen animation added

--- composeApp/src/commonMain/kotlin/com/ramki/movie/App.kt
+++ composeApp/src/commonMain/kotlin/com/ramki/movie/App.kt
@@ -1,30 +1,14 @@
 package com.ramki.movie
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
+import com.ramki.movie.presentation.navigation.AppNavGraph
 import com.ramki.movie.presentation.theme.AppTheme
-import movie_kmp.composeapp.generated.resources.Res
-import movie_kmp.composeapp.generated.resources.compose_multiplatform
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     AppTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-        }
         AppNavGraph()
     }
 }
--- composeApp/src/commonMain/kotlin/com/ramki/movie/presentation/screens/splash/SplashScreen.kt
+++ composeApp/src/commonMain/kotlin/com/ramki/movie/presentation/screens/splash/SplashScreen.kt
@@ -1,20 +1,31 @@
 package com.ramki.movie.presentation.screens.splash
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import movie_kmp.composeapp.generated.resources.Res
-import movie_kmp.composeapp.generated.resources.compose_multiplatform
-import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun SplashScreen(
@@ -32,10 +43,44 @@ fun SplashScreen(
 
 
 @Composable
-private fun SplashDetails(modifier: Modifier = Modifier) {
-    AnimatedVisibility(true) {
-        Column(modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Image(painterResource(Res.drawable.compose_multiplatform), null)
-        }
+private fun SplashDetails() {
+
+    var isAnimating by remember { mutableStateOf(false) }
+
+    val textSize by animateFloatAsState(
+        targetValue = if (isAnimating) 80f else 30f,
+        animationSpec = tween(durationMillis = 1000, easing = LinearEasing)
+    )
+
+    val gradientBrush = Brush.linearGradient(
+        colors = listOf(Color(0xFF6A1B9A), Color(0xFF4CAF50))
+    )
+
+    val rotationAngle by animateFloatAsState(
+        targetValue = if (isAnimating) 360f else 0f,
+        animationSpec = tween(durationMillis = 1000, easing = LinearEasing)
+    )
+
+    LaunchedEffect(Unit) {
+        isAnimating = true
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Movies",
+            modifier = Modifier
+                .graphicsLayer(rotationZ = rotationAngle)
+                .padding(8.dp),
+            style = TextStyle(
+                fontWeight = FontWeight.Bold,
+                fontSize = textSize.sp,
+                brush = gradientBrush
+            )
+        )
     }
 }
